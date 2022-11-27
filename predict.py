79c7b368a289d870e6885e45361dfa747e591293
Simson, Yonatan <yonatan.simson@intel.com>
2022-11-27 10:42:04 +0200

    Code cleanup

--- predict.py
+++ predict.py
@@ -1,36 +1,27 @@
 from __future__ import print_function
-import argparse
 import skimage
 import skimage.io
 import skimage.transform
 from PIL import Image
 import gc
-from math import log10
 
-import sys
-import shutil
 import os
 import torch
 from collections import OrderedDict
-import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
-import torch.optim as optim
 from torch.autograd import Variable
-from torch.utils.data import DataLoader
 from retrain.LEAStereo import LEAStereo
+from torchinfo import summary
 
 from config_utils.predict_args import obtain_predict_args
 from utils.colorize import get_color_map
+from utils.file_io import readPFM, save_pfm
 from utils.multadds_count import count_parameters_in_MB, comp_multadds
 from time import time
-from struct import unpack
 import matplotlib.pyplot as plt
-import re
 import numpy as np
-import pdb
 from path import Path
-
 
 
 def RGBToPyCmap(rgbdata):
@@ -53,75 +44,6 @@ def RGBToPyCmap(rgbdata):
                 'blue': bdata}
 
     return mpl_data
-
-
-# mpl_data = RGBToPyCmap(turbo_colormap_data)
-# plt.register_cmap(name='turbo', cmap=turbo_colormap_data.shape[0])
-
-def readPFM(file):
-    with open(file, "rb") as f:
-        # Line 1: PF=>RGB (3 channels), Pf=>Greyscale (1 channel)
-        type = f.readline().decode('latin-1')
-        if "PF" in type:
-            channels = 3
-        elif "Pf" in type:
-            channels = 1
-        else:
-            sys.exit(1)
-        # Line 2: width height
-        line = f.readline().decode('latin-1')
-        width, height = re.findall('\d+', line)
-        width = int(width)
-        height = int(height)
-
-        # Line 3: +ve number means big endian, negative means little endian
-        line = f.readline().decode('latin-1')
-        BigEndian = True
-        if "-" in line:
-            BigEndian = False
-        # Slurp all binary data
-        samples = width * height * channels;
-        buffer = f.read(samples * 4)
-        # Unpack floats with appropriate endianness
-        if BigEndian:
-            fmt = ">"
-        else:
-            fmt = "<"
-        fmt = fmt + str(samples) + "f"
-        img = unpack(fmt, buffer)
-        img = np.reshape(img, (height, width))
-        img = np.flipud(img)
-
-    return img, height, width
-
-
-def save_pfm(filename, image, scale=1):
-    '''
-    Save a Numpy array to a PFM file.
-    '''
-    color = None
-    file = open(filename, "w")
-    if image.dtype.name != 'float32':
-        raise Exception('Image dtype must be float32.')
-
-    if len(image.shape) == 3 and image.shape[2] == 3:  # color image
-        color = True
-    elif len(image.shape) == 2 or len(image.shape) == 3 and image.shape[2] == 1:  # greyscale
-        color = False
-    else:
-        raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
-
-    file.write('PF\n' if color else 'Pf\n')
-    file.write('%d %d\n' % (image.shape[1], image.shape[0]))
-
-    endian = image.dtype.byteorder
-
-    if endian == '<' or endian == '=' and sys.byteorder == 'little':
-        scale = -scale
-
-    file.write('%f\n' % scale)
-
-    image.tofile(file)
 
 
 def test_transform(temp_data, crop_height, crop_width):
@@ -290,10 +212,10 @@ def test_realsense(model, leftname, rightname, savename):
 
     plot_disparity(savename, temp, 192)
     savename_pfm = savename.replace('png', 'pfm')
+    save_pfm(savename_pfm, prediction)
     del input1, input2, prediction
     gc.collect()
     torch.cuda.empty_cache()
-    temp = np.flipud(temp)
 
 
 def plot_disparity(savename, data, max_disp):
@@ -318,7 +240,25 @@ if __name__ == "__main__":
     print('Matching Net Params = %.2fMB' % count_parameters_in_MB(model.matching))
 
     mult_adds = comp_multadds(model, input_size=(3, opt.crop_height, opt.crop_width))  # (3,192, 192))
-    print("compute_average_flops_cost = %.2fMB" % mult_adds)
+    print("compute_average_flops_cost = %.2fGB" % (mult_adds / 1e3))
+
+    input_size = (3, opt.crop_height, opt.crop_width)
+    input_size = (1,) + tuple(input_size)
+    # model = model.cuda()
+    input_data = torch.randn(input_size).cuda()
+
+    print("Torchinfo:")
+    summary_depth = 3
+    model_stats = summary(model,
+                          input_data=[input_data, input_data],
+                          col_names=("input_size", "output_size", "num_params", "mult_adds", "kernel_size"),
+                          depth=summary_depth)
+    summary_str = str(model_stats)
+    print(summary_str)
+    with open(f"../lea_stereo_torchinfo_depth{summary_depth}.txt", "w") as f:
+        f.write(summary_str)
+    gc.collect()
+    torch.cuda.empty_cache()
 
     if cuda:
         model = torch.nn.DataParallel(model).cuda()
